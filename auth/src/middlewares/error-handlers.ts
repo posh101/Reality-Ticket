29923d57d01296d19a91f97ab91a86340a91cb1a
posh101 <paulorife@gmail.com>
2024-01-26 01:28:04 -0700

    create a subclass error handlers

--- auth/src/middlewares/error-handlers.ts
+++ auth/src/middlewares/error-handlers.ts
@@ -1,4 +1,6 @@
 import {Request, Response, NextFunction} from "express"
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export const errorHandler = (
     err: Error, 
@@ -6,7 +8,22 @@ export const errorHandler = (
     res: Response,
      next: NextFunction
      ) => {
-      console.log("Something went wrong")
+      if(err instanceof RequestValidationError) {
+        const formattedErrors = err.errors.map(error => {
+          if(error.type === 'field') {
+            return {message: error.msg, field: error.path}
+          }
+        });
+        res.status(400).send({errors: formattedErrors})
+      }
 
-      res.status(400).send({message: err.message})
+      if(err instanceof DatabaseConnectionError) {
+        res.status(500).json({errors: [
+          {message: err.reason}
+        ]})
+      }
+
+      res.status(400).json({
+        errors: [{message: "Something went wrong"}]
+      });
 };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, {Request, Response} from "express"
 import {body, validationResult} from "express-validator"
+import { RequestValidationError } from "../errors/request-validation-error"
+import { DatabaseConnectionError } from "../errors/database-connection-error"
 
 const signupRouter = express.Router()
 
@@ -17,11 +19,11 @@ body('password')
   const errors = validationResult(req)
 
   if(!errors.isEmpty()) {
-    throw new Error('Invalid email or password')
+    throw new RequestValidationError(errors.array());
   }
  
   const {email, password} = req.body;
-  throw new Error('Error connecting to database...')
+  throw new  DatabaseConnectionError();
 
   console.log("Creating a user")
 
