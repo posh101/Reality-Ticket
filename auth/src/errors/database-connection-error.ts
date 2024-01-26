f1707915a69765b30352c87a2e6b8bc91c30fab2
posh101 <paulorife@gmail.com>
2024-01-26 13:59:21 -0700

    Create custom error and subclass

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,9 +1,17 @@
-export class DatabaseConnectionError extends Error {
-reason = "Error connecting to database"
+import { CustomError } from "./custom-errors";
+export class DatabaseConnectionError extends CustomError {
+statusCode = 500;
+reason = "Error connecting to database";
 
   constructor() {
     super();
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      { message: this.reason}
+    ]
   }
 }
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,7 +5,10 @@ import { currentUserRouter } from "./routes/current-user"
 import { signinRouter } from "./routes/signin"
 import { signoutRouter } from "./routes/signout"
 import { signupRouter } from "./routes/signup"
+
+//Import error handler
 import { errorHandler } from "./middlewares/error-handlers"
+import { NotFoundError } from "./errors/not-found-error"
 
 const app = express();
 app.use(express.json());
@@ -16,6 +19,11 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.all('*', () => {
+    throw new NotFoundError();
+});
+
+//Error Handler middleware
 app.use(errorHandler);
 
 app.listen(1000, () => {
