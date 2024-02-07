e298b7df8929bf73d5b2e932468577a50c94428e
posh101 <paulorife@gmail.com>
2024-02-07 09:59:03 -0700

    Connect to mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,9 @@
-import  express from "express"
+import  express from "express";
+import 'express-async-errors';
+import  mongoose from "mongoose"
+import { ConnectOptions } from "mongoose";
+import dotenv from "dotenv";
+
 
 //Import Route Handlers
 import { currentUserRouter } from "./routes/current-user"
@@ -9,6 +14,7 @@ import { signupRouter } from "./routes/signup"
 //Import error handler
 import { errorHandler } from "./middlewares/error-handlers"
 import { NotFoundError } from "./errors/not-found-error"
+import { DatabaseConnectionError } from "./errors/database-connection-error";
 
 const app = express();
 app.use(express.json());
@@ -19,13 +25,23 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all('*', () => {
+app.all('*', async () => {
     throw new NotFoundError();
 });
 
 //Error Handler middleware
 app.use(errorHandler);
 
-app.listen(1000, () => {
-    console.log("listening on port:1000")
-})
+//Connect to enviroment variable
+dotenv.config();
+const PORT = process.env.PORT || 8000;
+
+//Connectig to mongodb
+const url = process.env.MONGO_URL
+mongoose.connect(url,)
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.log(err.message))
+
+    app.listen(PORT, () => {
+        console.log(`Listening on port: ${PORT}`)
+    });
